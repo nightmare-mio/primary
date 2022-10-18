c9772b9fad8f7addb5ce08696689a025819cac2b
wanglongwei <wanglongwei@yuqiaotech.com>
2022-10-18 09:38:40 +0800

    升级vite

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,8 @@
-const baseURL = "http://127.0.0.1:8081";
-// const baseURL = "http://42.193.248.221:8081";
+const ENV="dev";
+if (NEV=="dev") {
+    const baseURL = "http://127.0.0.1:8081";
+} else if(NEV=="test") {
+    const baseURL = "http://42.193.248.221:8081";
+}
 
 export { baseURL }
